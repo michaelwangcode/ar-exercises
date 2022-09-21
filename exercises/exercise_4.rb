4cdef94ee29dd61a7941d025571656cf306d06f7
michaelwangcode <michaelwangxv@gmail.com>
2022-09-21 04:02:18 -0400

    Exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,39 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+# Create three more stores
+
+Store.create(name: "Surrey",
+            annual_revenue: 224000,
+            mens_apparel: false,
+            womens_apparel: true)
+
+Store.create(name: "Whistler",
+            annual_revenue: 1900000,
+            mens_apparel: true,
+            womens_apparel: false)
+
+Store.create(name: "Yaletown",
+            annual_revenue: 430000,
+            mens_apparel: true,
+            womens_apparel: true)
+
+
+# Fetch a collection of stores that sell mens apparel
+@mens_apparel_stores = Store.where(mens_apparel: true)
+
+# Loop through the mens stores and print the name and revenue
+@mens_apparel_stores.each do |store|
+  puts store.name
+  puts store.annual_revenue
+end
+
+
+# Fetch a collection of stores that sell womens apparel
+@womens_apparel_stores = Store.where(womens_apparel: true).where("annual_revenue < 1000000")
+
+# Loop through the mens stores and print the name and revenue
+@womens_apparel_stores.each do |store|
+  puts store.name
+  puts store.annual_revenue
+end
