f67b9b6ce7225f499a6cdfd5c845d27eefb2fff3
michaelwangcode <michaelwangxv@gmail.com>
2022-09-21 03:35:56 -0400

    Exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,13 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+
+# Assign the two stores to instance variables
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+# Change the first store's name
+@store1.update(name: "New Burnaby")
+
+# Print the first store's new name
+puts @store1.name
