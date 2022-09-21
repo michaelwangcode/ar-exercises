2c96df86a741d70213b41408873fea30bf0d6535
michaelwangcode <michaelwangxv@gmail.com>
2022-09-21 06:07:40 -0400

    Exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,13 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+
+# Get store name from user
+puts "Enter store name:"
+@store_name = gets.chomp
+
+# Create new store with only name
+new_store = Store.create(name: @store_name)
+
+# Print error messages from trying to create the new store
+puts new_store.errors.full_messages
