6ab9ce9e831e8800cdd8d55ca2c1851247ba5ba9
michaelwangcode <michaelwangxv@gmail.com>
2022-09-21 03:44:41 -0400

    Exercise 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -5,4 +5,11 @@ require_relative './exercise_2'
 puts "Exercise 3"
 puts "----------"
 
-# Your code goes here ...
+# Assign the third store into an instance variable
+@store3 = Store.find(3)
+
+# Delete store 3 using the destroy method
+@store3.destroy
+
+# Print the number of stores
+puts Store.count
